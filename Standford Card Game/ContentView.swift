818ec701ad3d0443291474a3ee0286835b044092
Karl Kuhn <karl@karlkuhn.co>
2022-07-10 13:49:40 -0400

    Worked through Lecture 2of Stanford CS193P
    Learned about:
    
    ForEach(Array<>[RANGE], id\.self { arg in ....... }
    
    CLEAN CODE - using "some View" vars to pull out bulky code:
    
    var add: some View {
    	return Button { .....

--- Standford Card Game/ContentView.swift
+++ Standford Card Game/ContentView.swift
@@ -8,21 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var emojiCount = 20
+    var emojis = ["🚢", "🚈", "🦼", "🚤", "🚚", "🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚓", "🚑", "🚒", "🚐", "🛻", "🚛", "🚜", "🚲", "🛵", "🏍", "🛺", "🚔", "🚘"]
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello World")
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.red)
+            }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
         }
         .padding(.horizontal)
-        .foregroundColor(.red)
         
     }
     
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+
+    }
+    
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.portraitUpsideDown)
+        ContentView()
+            .preferredColorScheme(.light)
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
