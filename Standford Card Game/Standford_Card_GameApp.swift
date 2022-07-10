ef2e8656789c74cce3453a0c65f57279dc0bbea7
Karl Kuhn <karl@karlkuhn.co>
2022-07-09 23:56:25 -0400

    Initial Commit

---
+++ Standford Card Game/Standford_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Standford_Card_GameApp.swift
+//  Standford Card Game
+//
+//  Created by Karl Kuhn on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Standford_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
